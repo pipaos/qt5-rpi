b98fac4c38234fb0887cc7fc37861c61da16e940
Albert Casals <skarbat@gmail.com>
2017-01-03 15:08:28 +0000

    Command line options, automation in dry_run, cross and native build

--- build/builder.py
+++ build/builder.py
@@ -13,9 +13,12 @@ import xsysroot
 
 class Builder():
 
-    def __init__(self, config_file='qt5-configuration.json'):
+    def __init__(self, config_file='qt5-configuration.json', cross=True, release=True, dry_run=True):
         self.config = json.loads(open(config_file, 'r').read())
         self.sysroot = xsysroot.XSysroot(profile=self.config['xsysroot_profile'])
+        self.cross=cross
+        self.release=release
+        self.dry_run=dry_run
         self._complete_config()
 
     def _complete_config(self):
@@ -25,35 +28,50 @@ class Builder():
         self.config['configure_release'] = self.config['configure_release'].format(**self.config)
         self.config['configure_debug'] = self.config['configure_debug'].format(**self.config)
         self.config['sources_directory'] ='{}/{}'.format(self.sysroot.query('tmp'), self.config['qt5_clone_dir'])
+        self.config['cross_install_dir']='{}/{}'.format(self.sysroot.query('sysroot'), self.config['qt5_install_prefix'])
+        self.config['qt5_cross_qt_conf']='{sysroot}/{qt5_install_prefix}/{qt5_cross_binaries}/qt.conf'.format(**self.config)
         self.host_numcpus=multiprocessing.cpu_count()
 
     def are_sources_cloned(self):
         return os.path.isdir(self.config['sources_directory'])
-        
+
+    def is_qt5_installed(self):
+        return os.path.isdir(self.config['cross_install_dir'])
+
+    def are_cross_tools_built(self):
+        x64bins='{cross_install_dir}/{qt5_cross_binaries}'.format(**self.config)
+        return os.path.isdir(x64bins)
+
     def dump_configuration(self):
-        pprint.pprint(self.config)
+        pprint.pprint(self.config, indent=2)
 
     def status(self):
         print 'sysroot mounted:', self.sysroot.is_mounted()
+        print 'QT5 sources cloned:', self.are_sources_cloned()
+        print 'QT5 installed:', self.is_qt5_installed()
+        print 'QT5 cross tools built:', self.are_cross_tools_built()
+
 
 class BuilderQt5(Builder):
     
     def clone_repos(self):
         if self.are_sources_cloned():
-            print 'QT5 sources already cloned'
+            print 'QT5 sources already cloned, continuing'
             return True
 
-        command = 'git clone --branch={} {} {}'.format(
-            self.config['qt5_version'],
-            self.config['qt5_repo_url'],
-            self.config['sources_directory'])
-        
-        if os.system(command):
+        git_command = 'git clone --branch={qt5_version} {qt5_repo_url} {sources_directory}'.format(**self.config)
+        init_command='cd {sources_directory} ; perl ./init-repository'.format(**self.config)
+
+        if self.dry_run:
+            print '>>> ', git_command
+            print '>>> ', init_command
+            return True
+
+        if os.system(git_command):
             print 'Error cloning sources'
             return False
 
-        command='cd {} ; perl ./init-repository'.format(self.config['sources_directory'])
-        if os.system(command):
+        if os.system(init_command):
             print 'Error initializing repository'
             return False
         
@@ -63,6 +81,10 @@ class BuilderQt5(Builder):
         '''
         Prepare the image for the first time.
         '''
+        print '>>> baptizing image'
+        if self.dry_run:
+            return True
+
         # make sure the image is not currently in use
         if self.sysroot.is_mounted():
             if not self.sysroot.umount():
@@ -83,16 +105,22 @@ class BuilderQt5(Builder):
     def install_dependencies(self):
         # Put the system up to date and install QT5 build dependencies
         # We might need more, for example TLS and further backends
+        command='apt-get install -y --no-install-recommends {sysroot_dependencies}'.format(**self.config)
+        print '>>> installing sysroot dependencies'
+        if self.dry_run:
+            print '>>>', command
+            return True
+
         if self.sysroot.execute('apt-get update'):
             return False
 
-        if self.sysroot.execute('apt-get install -y --no-install-recommends {}'.format(
-                self.config['sysroot_dependencies'])):
+        if self.sysroot.execute(command):
             return False
 
+        self._fix_qualified_paths()
         return True
 
-    def fix_qualified_paths(self):
+    def _fix_qualified_paths(self):
         # Fix relative symlinks to libdl and libm (so called fixQualifiedPaths in QT jargon)
         # TODO: Use readlink instead of hardcoded destination to .so versioned filename,
         #       but no big deal really. This is a development / builder box.
@@ -110,16 +138,66 @@ class BuilderQt5(Builder):
                        self.sysroot.query('sysroot')))
 
     def configure(self):
-        command='cd {} && ./configure {}'.format(self.config['sources_directory'], self.config['configure_release'])
+        configure_opts=self.config['configure_release'] if self.release else self.config['configure_debug']
+        if self.cross:
+            command='cd {} && ./configure {}'.format(self.config['sources_directory'], configure_opts)
+        else:
+            command='xsysroot -x "cd /tmp/{} && ./configure {}"'.format(self.config['qt5_clone_dir'], configure_opts)
+
+        if self.dry_run:
+            print '>>>', command
+            return True
+
         rc = os.system(command)
         return os.WEXITSTATUS(rc) == 0
 
     def make(self):
-        command='cd {} && make -j {}'.format(self.config['sources_directory'], self.config['num_cpus'])
+        if self.cross:
+            command='cd {sources_directory} && make -j {num_cpus}'.format(**self.config)
+        else:
+            command='xsysroot -x "cd /tmp/{qt5_clone_dir} && make -j {num_cpus}"'.format(**self.config)
+
+        if self.dry_run:
+            print '>>>', command
+            return True
+
         rc = os.system(command)
         return os.WEXITSTATUS(rc) == 0
 
     def install(self):
-        command='cd {} && sudo make install'.format(self.config['sources_directory'])
+        # TODO create a qt.conf file
+        qtconfig='[Paths]\n' \
+            'Sysroot = {sysroot}\n' \
+            'Prefix = {qt5_install_prefix}\n' \
+            'HostPrefix= {sysroot}/{qt5_install_prefix}\n' \
+            'Binaries = {qt5_cross_binaries}\n' \
+            'HostBinaries = {qt5_cross_binaries}\n'.format(**self.config)
+
+        if self.cross:
+            command='cd {sources_directory} && sudo make install && ' \
+                'sudo mv -fv {cross_install_dir}/bin {cross_install_dir}/{qt5_cross_binaries}'.format(**self.config)
+        else:
+            command='xsysroot -x "cd /tmp/{qt5_clone_dir} && make install'.format(**self.config)
+
+        if self.dry_run:
+            print '>>>', command
+            print 'qtconfig >>>\n', qtconfig
+            return True
+
         rc = os.system(command)
+        if not rc and self.cross:
+            # TODO: Make this simpler
+            qtconfig_file='{qt5_cross_qt_conf}'.format(**self.config)
+            qtconfig_temp='/tmp/qtconfig'
+            with open (qtconfig_temp, 'w') as f:
+                f.write(qtconfig)
+
+            os.system('sudo cp -fv /tmp/qtconfig {qt5_cross_qt_conf}'.format(**self.config))
+
         return os.WEXITSTATUS(rc) == 0
+
+
+class BuilderWebengine(Builder):
+
+    def haw(self):
+        pass
